f1742b8968ffc51fcebe0517f0953c67d08de939
Raul Guilherme <contato.raulguilherme@gmail.com>
2025-02-11 13:15:58 -0300

    Add files via upload

---
+++ firstcontactwithc#.cs
@@ -0,0 +1,60 @@
+﻿string msgbv = @"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+";
+
+void ExibirMsBv()
+{
+    Console.WriteLine(msgbv);
+}
+
+ExibirMsBv();
+//https://fsymbols.com
+
+void ExibirOpMenu()
+{
+    Console.WriteLine("Digite 1 para regristrar uma banda");
+    Console.WriteLine("Digite 2 para Mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para Avaliar uma Banda");
+    Console.WriteLine("Digite 4 para Exibir a media de uma banda");
+    Console.WriteLine("Digite 0 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string OpEsc = Console.ReadLine()!;
+    int OpEscNum = int.Parse(OpEsc);
+
+
+    switch (OpEscNum)
+    {
+        case 1: Console.WriteLine("Você escolhe a opção: " + OpEscNum);
+            break;
+        case 2:
+            Console.WriteLine("Você escolhe a opção: " + OpEscNum);
+            break;
+        case 3:
+            Console.WriteLine("Você escolhe a opção: " + OpEscNum);
+            break;
+        case 4:
+            Console.WriteLine("Você escolhe a opção: " + OpEscNum);
+            break;
+        case 0:
+            Console.WriteLine(@"░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░" + "\nVolte sempre!");
+            break;
+        default: 
+            Console.WriteLine("Opção invalida, insira uma opção válida");
+            break;
+
+    }
+
+}
+
+ExibirOpMenu();
